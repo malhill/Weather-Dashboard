9f82e9a1807e81dbaa248e1a174cc5731bf7864d
malhill <74620676+malhill@users.noreply.github.com>
2021-02-01 01:07:27 -0500

    updated readme and cleaned comments

--- README.md
+++ README.md
@@ -3,14 +3,17 @@ The goal here is to use multiple API's (5 day forecast, UV, and Current day) to 
 
 # HTML
 * Creating a grid structure to seperate the list, weather cards, and weather data by divs. 
+* Will need to practice the grid formatting and organizing the divs!
 
 # CSS
-
+* I experimented with the CSS here so that I could visually see the divs in different colors. It helped me seperate the five day forecast in a horizontal line. 
 
 # JS
 * Multiple AJAX
 * Worked on creating and appending classes to the app
 * I tried going with the idea of commenting on everything as if im talking to myself for clarity
+* Went from nesting my ajax functions to seperating everything and calling the functions seperately. Must easier to understand/test.
+* Again trying to understand different processes that I am referencing via google. Then taking the rules and applying them for myself. Good lookiing code means nothing without application. 
 
 # Creators Note:
 * Putting an AJAX within another AJAX was complex for my brain
--- function.js
+++ function.js
@@ -29,7 +29,6 @@ function showSearched(city) {
     searchBar.prepend(li);
 }
 
-
 //Pre-pending, user input. Saving last searched item
 function citySearching() {
     const citySearch = localStorage.getItem("cityName");
@@ -37,16 +36,9 @@ function citySearching() {
 }
 citySearching();
 
-
 function getWeather(cityName) {
     //Current and 5 day forecast constants
     const apiWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&Appid=" + apiKey + "&units=imperial";
-
-    //If no data is called 
-    // if (textData == "") {
-    //     return;
-    // }   else {
-    // console.log($("#city-input"));
 
     //Typical AJAX
     $.ajax({
@@ -55,31 +47,21 @@ function getWeather(cityName) {
     }).then(function (response) {
         console.log(response);
 
-        // const weatherCard = $(".weather-card").append("<div>").addclass("weather-body")
-        //So card doent retain old info!
-        // weatherCard.empty();
-        // weatherCard.html('');
-
         // Select div where we will append the info and empty it
         currentWeatherDiv.empty();
-        // get date, city name, temp, humidity, speed and icon and set them to variables
-        // append them to currentWeatherDiv
-
-        //const currentTime = weatherCard.append("<p>");
-        // Going to add class 
-        // weatherCard.append(currentTime);
-
+        
         //Time next, aiming to try out a timing template
         const dateTime = new Date(response.dt * 1000);
         const currentDate = $('<p>').text(dateTime.toLocaleDateString('en-US'));
         console.log(currentWeatherDiv);
 
+        // Temperature + response of the main temp in a paragraph
         let currentTemp = ("<p>" + "Temperature: " + response.main.temp + "</p>");
-        // Setting Humidity
+        // Humidity + response of the main humidity in a paragraph
         let currentHumidity = ("<p>" + "Humidity: " + response.main.humidity + "%" + "</p>");
         // Wind (lol I can't stop thinking captain planet)
         let currentWind = ("<p>" + "Wind Speed: " + response.wind.speed + "</p>");
-
+        // The icons here will be used in the card format to display what the weather will look like. Trikcy with the $ usage inside the https
         let currentIcon = (`<img src="https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
 
         currentWeatherDiv.append(currentDate, currentTemp, currentHumidity, currentWind, currentIcon);
@@ -98,14 +80,14 @@ function weatherSearch(event) {
     // if (event.target.matches)
     // console.log("Hi");
     getWeather(textData)
-
-
 }
 
+// Working towards making the five cards
 function fiveDayForecast(cityName) {
 
     const apiFive = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&Appid=" + apiKey + "&units=imperial";
-
+    
+    //Typical AJAX
     $.ajax({
         url: apiFive,
         method: "GET"
@@ -121,9 +103,10 @@ function fiveDayForecast(cityName) {
         // For each for 5 days
         for (let i = 4; i < day.length; i += 8) {
 
+            // Making a div with 2 classes to append to later
             let fiveDayCard = $("<div>").addClass("card fiveDayColor");
             let FiveDayTime = new Date(response.list[i].dt * 1000);
-
+            // Setting string to five day time 
             FiveDayTime = FiveDayTime.toLocaleDateString("en-US");
             fiveDayCard.html("<p>" + FiveDayTime + "</p>" + `<img src='https://openweathermap.org/img/wn/${response.list[i].weather[0].icon}@2x.png'>` + "<p>" + "Temperature: " + response.list[i].main.temp + "</p>" + "<p>" + "Humidity: " + response.list[i].main.humidity + "%" + "</p>")
 
@@ -132,8 +115,7 @@ function fiveDayForecast(cityName) {
     });
 }
 
-// Aiming for UV AJAX "crosses fingers"
-
+// Aiming for UV AJAX *crosses fingers*
 function getUV(lat, long) {
 
     const UV = `https://api.openweathermap.org/data/2.5/uvi?appid=b8ecb570e32c2e5042581abd004b71bb&lat=${lat}&lon=${long}&appip=${apiKey}`;
